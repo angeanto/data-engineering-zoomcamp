648383af2dc24c7effa80c85e088098c18853731
GCP Antonis <antonis@de-zoomcamp.europe-west6-a.c.adept-storm-375515.internal>
2023-02-05 15:59:54 +0000

    Modify gitignore

--- week_2_workflow_orchestration/flows/02_gcp/etl_gcs_to_bq.py
+++ week_2_workflow_orchestration/flows/02_gcp/etl_gcs_to_bq.py
@@ -19,20 +19,18 @@ def transform(path: Path) -> pd.DataFrame:
     """Data cleaning example"""
     df = pd.read_parquet(path)
     print(f"pre: missing passenger count: {df['passenger_count'].isna().sum()}")
-    df["passenger_count"].fillna(0, inplace=True)
-    print(f"post: missing passenger count: {df['passenger_count'].isna().sum()}")
     return df
 
 
 @task()
-def write_bq(df: pd.DataFrame) -> None:
+def write_bq(df: pd.DataFrame,color: str) -> None:
     """Write DataFrame to BiqQuery"""
 
     gcp_credentials_block = GcpCredentials.load("zoom-gcp-creds")
 
     df.to_gbq(
-        destination_table="dezoomcamp.rides",
-        project_id="prefect-sbx-community-eng",
+        destination_table= f"dezoomcamp.rides_{color}",
+        project_id="adept-storm-375515",
         credentials=gcp_credentials_block.get_credentials_from_service_account(),
         chunksize=500_000,
         if_exists="append",
@@ -42,14 +40,30 @@ def write_bq(df: pd.DataFrame) -> None:
 @flow()
 def etl_gcs_to_bq():
     """Main ETL flow to load data into Big Query"""
-    color = "yellow"
-    year = 2021
-    month = 1
+    color = ["yellow","green"]
+    total_rows_green = 0
+    total_rows_yellow = 0 
+    for x in color: 
+        if x == "green":
+            year = 2020
+            month = 1
+            path = extract_from_gcs(x, year, month)
+            df = transform(path)
+            write_bq(df,x)
+            total_rows_green = total_rows_green + len(df)
+        elif x == "yellow":
+            year = 2019
+            month = [2,3]
+            for y in month:
+                path = extract_from_gcs(x, year, y)
+                df = transform(path)
+                write_bq(df,x)
+                total_rows_yellow = total_rows_yellow + len(df)
+        else:
+            raise Exception("Sorry, there's no such file")
 
-    path = extract_from_gcs(color, year, month)
-    df = transform(path)
-    write_bq(df)
-
+    print('Total processed rows for yellow:', total_rows_yellow)
+    print('Total processed rows for green:', total_rows_green)
 
 if __name__ == "__main__":
     etl_gcs_to_bq()
